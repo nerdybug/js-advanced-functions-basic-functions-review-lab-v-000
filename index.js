e987095f7349de4dafae8adf1ee39033ab0c2c76
Meredith Reece <mardebugger@gmail.com>
2021-03-25 20:52:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair="*") {
+  return function (msg="special") {
+    return `You are ${flair}${msg}${flair}!`;
+  }
+};
+
+var Calculator = {
+  add : function(x, y) {
+    return x + y;
+  },
+  subtract : function(x, y) {
+    return x - y;
+  },
+  multiply: function(x, y) {
+    return x * y;
+  },
+  divide : function(x, y) {
+    return x / y;
+  }
+};
+
+function actionApplyer(startInt, array=[]) {
+  if (array.empty?) {
+    return startInt;
+  }
+};
